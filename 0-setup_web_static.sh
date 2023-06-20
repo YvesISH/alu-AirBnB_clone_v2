d7aabcc110ce882aab6c52097b12918df24b5fb3
root <y.ishimwe2@alustudent.com>
2023-06-20 02:33:08 -0700

    Preparing Web-Servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#web_static development
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/we_static/current
+sido chown -hR ubuntu:ubuntu /data/
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx start
