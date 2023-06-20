7f855c967b72a5da4ef3220168c270324e3b3804
root <y.ishimwe2@alustudent.com>
2023-06-20 02:42:21 -0700

    Preparing Web-Servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,4 +9,5 @@ echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/tes
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
