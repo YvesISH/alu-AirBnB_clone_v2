200774671c5cc35d9efec43b50d562b0571f2b45
root <y.ishimwe2@alustudent.com>
2023-06-22 01:02:27 -0700

    Deploy Archiive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""Fabric Script that distributes an archives"""
-
-
-from fabric.api import put, run, env
+"""Fabric script that distributes an archive to your web servers"""
+from fabric.api import env, put, run
 from os.path import exists
+
 env.hosts = ['54.82.5.102', '3.94.103.18']
+env.user = "ubuntu"
+env.key = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
-    """Archives to web-servers"""
-    if exists(archive_path) is False:
+    """Function to distribute an archive to your web servers"""
+    if not exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
         return True
-    except:
+    except Exception:
         return False
