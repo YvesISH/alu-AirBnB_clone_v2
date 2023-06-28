cef45912e26aead2d074efca40f3f59f6af8f6dd
root <y.ishimwe2@alustudent.com>
2023-06-28 09:27:23 -0700

    Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Flask webApp that listens on 0.0.0.0"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
